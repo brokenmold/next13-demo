5900dcc152d221202299dadf44fad19497f88f5c
Jason Glass <jason@broken-mold.com>
2022-11-02 12:25:28 -0500

    Initial Exploration & Cleanup

--- app/page.jsx
+++ app/page.jsx
@@ -4,41 +4,37 @@ import styles from './page.module.scss'
 export default function Home() {
   return (
     <div className={styles.container}>
+
       <main className={styles.main}>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js 13!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
+          <code className={styles.code}>app/page.jsx</code>
         </p>
 
+
         <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
+
+          {/* Docs */}
+          <a className={styles.card} href="https://beta.nextjs.org/docs">
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
+            <p>Interactive Beta Docs for Next.js 13+</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
+          {/* SCSS Modules */}
+          <a className={styles.card} href="https://beta.nextjs.org/docs/styling/sass">
+            <h2>SCSS Modules &rarr;</h2>
+            <p>Component-level SASS/SCSS via CSS Modules</p>
           </a>
 
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
         </div>
+
       </main>
+
 
       <footer className={styles.footer}>
         <a
@@ -52,6 +48,7 @@ export default function Home() {
           </span>
         </a>
       </footer>
+
     </div>
   )
 }
